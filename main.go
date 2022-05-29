87740c324968877cd87a129249d7d71cae05f6b4
Hadi Rezaei <hadirezaei.etc.esfahan@gmail.com>
2022-05-29 21:06:34 +0430

    more details

--- main.go
+++ main.go
@@ -1,14 +1,25 @@
 package main
 
+//Writing a basic HTTP server using the net/http package.
+
 import (
 	"fmt"
 	"net/http"
 )
 
+//A fundamental concept in net/http servers is handlers.
+//A handler is an object implementing the http.Handler interface.
+// A common way to write a handler is by using the http.
+//HandlerFunc adapter on functions with the appropriate signature.
+
 func hello(w http.ResponseWriter, req *http.Request) {
 
 	fmt.Fprintf(w, "hello\n")
 }
+
+//Functions serving as handlers take a http.ResponseWriter and a http.Request as arguments.
+//The response writer is used to fill in the HTTP response.
+// Here our simple response is just “hello\n”.
 
 func headers(w http.ResponseWriter, req *http.Request) {
 
@@ -19,10 +30,24 @@ func headers(w http.ResponseWriter, req *http.Request) {
 	}
 }
 
+//This handler does something a little more sophisticated by
+// reading all the HTTP request headers and echoing them into the response body.
+
 func main() {
+
+	//We register our handlers on server routes using the http.
+	//HandleFunc convenience function. It sets up the default router in the
+	// net/http package and takes a function as an argument.
 
 	http.HandleFunc("/hello", hello)
 	http.HandleFunc("/headers", headers)
 
 	http.ListenAndServe(":8090", nil)
 }
+
+//Finally, we call the ListenAndServe with the port and a handler. nil tells it to use the default router we’ve just set up.
+
+//output :
+
+//curl localhost:8090/hello
+//hello
